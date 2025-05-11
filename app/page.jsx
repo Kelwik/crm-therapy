5c69fdbc6ce3741edff4ef832c2144b0a154de10
Alwi Kasim <alwikasim321@gmail.com>
2025-05-11 21:59:42 +0800

    add dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { redirect } from 'next/navigation';
 import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Users, Mail, TriangleAlert } from 'lucide-react';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
@@ -32,9 +34,42 @@ export default function Home() {
 
   if (isLoading) return <p>Loading...</p>;
   return (
-    <div>
-      <p>This is the dashboard</p>
-      <Button onClick={logOut}>Log out</Button>
+    <div className="w-screen h-screen bg-gray-200">
+      <nav className="bg-white p-4 flex flex-col items-center shadow">
+        <p className="font-medium text-3xl">Therapy CRM</p>
+        <p className="font-normal text-gray-500">Patient Dashboard</p>
+      </nav>
+      <div className="mx-16 mt-4">
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardContent className="flex flex-row gap-6 items-center">
+              <Users size={48} />
+              <div>
+                <p className="text-2xl text-gray-500">Total Pasien</p>
+                <p className="text-3xl font-medium">42</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="flex flex-row gap-6 items-center">
+              <Mail size={48} />
+              <div>
+                <p className="text-2xl text-gray-500">Email Belum Direspons</p>
+                <p className="text-3xl font-medium">42</p>
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="flex flex-row gap-6 items-center">
+              <TriangleAlert size={48} />
+              <div>
+                <p className="text-2xl text-gray-500">Pasien Butuh Follow Up</p>
+                <p className="text-3xl font-medium">42</p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
