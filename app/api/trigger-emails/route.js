8b7eb1be17dd90c4ead1d26fded1d6bc6e656e18
Alwi Kasim <alwikasim321@gmail.com>
2025-05-13 13:14:35 +0800

    fix api link

--- app/api/trigger-emails/route.js
+++ app/api/trigger-emails/route.js
@@ -2,12 +2,15 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   try {
-    const response = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(
+      'https://crm-therapy.vercel.app/api/send-email',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
     const data = await response.json();
     return NextResponse.json(data, { status: response.status });
